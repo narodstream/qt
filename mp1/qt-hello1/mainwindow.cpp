e1307bd268ec374c3f45a99c81f0ccc390d74d4e
narodstream <vlad_vorobyev@mail.ru>
2020-02-25 07:26:11 +0300

    upd202002250726

--- mp1/qt-hello1/mainwindow.cpp
+++ mp1/qt-hello1/mainwindow.cpp
@@ -25,6 +25,15 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //setWindowFlags( Qt::Window | Qt::CustomizeWindowHint | Qt::WindowMinMaxButtonsHint); //убираем все кнопки
+    timer = new QTimer();
+    connect(timer, SIGNAL(timeout()), this, SLOT(slotTimerAlarm()));
+    ui->pushButtonClose->setEnabled(false);
+    ui->pushButtonTest->setEnabled(false);
+    ui->pushButtonInit->setEnabled(false);
+    ui->pushButtonString->setEnabled(false);
+    ui->pushButtonClear->setEnabled(false);
+    ui->pushButtonPos->setEnabled(false);
 }
 
 MainWindow::~MainWindow()
@@ -32,16 +41,25 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
+void MainWindow::on_pushButtonInit_clicked()
 {
     char c;
     char str1[30];
+    cnt01 = 0;
+    isStarted = false;
     c = ioctl(fd, IOCTL_GET_NTH_BYTE, 0);
     sprintf(str1, "mygpio char: %d", (int) c);
     ui->label->setText(str1);
+    //Clear Display
+    ioctl(fd, IOCTL_LCD_CLEAR, 0);
+    ui->pushButtonTest->setEnabled(true);
+    ui->pushButtonString->setEnabled(true);
+    ui->pushButtonClear->setEnabled(true);
+    ui->pushButtonPos->setEnabled(true);
+    ui->pushButtonInit->setEnabled(false);
 }
 
-void MainWindow::on_pushButton_3_clicked()
+void MainWindow::on_pushButtonClose_clicked()
 {
     int err;
     err = closeFile(fd);
@@ -52,10 +70,17 @@ void MainWindow::on_pushButton_3_clicked()
     else
     {
         ui->label->setText("mygpio close");
+        ui->pushButtonExit->setEnabled(true);
+        ui->pushButtonOpen->setEnabled(true);
+        ui->pushButtonString->setEnabled(false);
+        ui->pushButtonInit->setEnabled(false);
+        ui->pushButtonClear->setEnabled(false);
+        ui->pushButtonTest->setEnabled(false);
+        ui->pushButtonPos->setEnabled(false);
     }
 }
 
-void MainWindow::on_pushButton_2_clicked()
+void MainWindow::on_pushButtonOpen_clicked()
 {
     fd = open("/dev/mygpio", O_RDWR);
     if (fd < 0)
@@ -65,19 +90,23 @@ void MainWindow::on_pushButton_2_clicked()
     else
     {
         ui->label->setText("mygpio open");
+        ui->pushButtonExit->setEnabled(false);
+        ui->pushButtonOpen->setEnabled(false);
+        ui->pushButtonClose->setEnabled(true);
+        ui->pushButtonInit->setEnabled(true);
     }
 }
 
-void MainWindow::on_pushButton_4_clicked()
+void MainWindow::on_pushButtonString_clicked()
 {
     char str1[30];
     //ui->label->setText(ui->lineEdit->text());
     sprintf(str1, ui->lineEdit->text().toStdString().c_str());
     ui->label->setText(str1);
-    write(fd,str1,strlen(str1));
+    write(fd,str1,strlen(str1)+1);
 }
 
-void MainWindow::on_pushButton_5_clicked()
+void MainWindow::on_pushButtonClear_clicked()
 {
     char c;
     char str1[30];
@@ -86,11 +115,70 @@ void MainWindow::on_pushButton_5_clicked()
     ui->label->setText(str1);
 }
 
-void MainWindow::on_pushButton_6_clicked()
+void MainWindow::on_pushButtonPos_clicked()
 {
     char c;
     char str1[30];
     c = ioctl(fd, IOCTL_LCD_SET_POS, ui->spinBox1->value() | ui->spinBox2->value() << 8);
-    sprintf(str1, "mygpio char: %d", (int) c);
+    sprintf(str1, "mygpio char: %d; x=%d; y=%d", (int) c, ui->spinBox1->value(), ui->spinBox2->value());
     ui->label->setText(str1);
+}
+
+void MainWindow::slotTimerAlarm()
+{
+    //ui->label->setText(QTime::currentTime().toString("hh:mm:ss"));
+    char str1[30];
+    cnt01++;
+    if(cnt01>=200000) cnt01=0;
+    if(cnt01%2)
+    {
+        ioctl(fd, IOCTL_LCD_SET_POS, 4 | 3 << 8);
+        strcpy(str1,QTime::currentTime().toString("hh:mm:ss").toLocal8Bit().data());
+        write(fd,str1,9);
+    }
+    else
+    {
+        ioctl(fd, IOCTL_LCD_SET_POS, 6 | 2 << 8);
+        //usleep(5000);
+        sprintf(str1,"%5d", cnt01/2);
+        write(fd,str1,6);
+    }
+}
+
+void MainWindow::on_pushButtonExit_clicked()
+{
+    QApplication::quit();
+}
+
+void MainWindow::closeEvent(QCloseEvent *event){
+    event->ignore();
+}
+
+void MainWindow::on_pushButtonTest_clicked()
+{
+    if(isStarted == false)
+    {
+        //Clear Display
+        ioctl(fd, IOCTL_LCD_CLEAR, 0);
+        ui->pushButtonTest->setText("Test stop");
+        isStarted = true;
+        ui->pushButtonString->setEnabled(false);
+        ui->pushButtonClear->setEnabled(false);
+        ui->pushButtonPos->setEnabled(false);
+        ui->pushButtonClose->setEnabled(false);
+        timer->start(100);
+    }
+    else
+    {
+        ui->pushButtonTest->setText("Test start");
+        isStarted = false;
+        timer->stop();
+        cnt01 = 0;
+        //Clear Display
+        ioctl(fd, IOCTL_LCD_CLEAR, 0);
+        ui->pushButtonString->setEnabled(true);
+        ui->pushButtonClear->setEnabled(true);
+        ui->pushButtonPos->setEnabled(true);
+        ui->pushButtonClose->setEnabled(true);
+    }
 }
--- mp1/qt-hello1/mainwindow.h
+++ mp1/qt-hello1/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QCloseEvent>
+#include <QTimer>
+#include <QTime>
 
 namespace Ui {
 class MainWindow;
@@ -13,24 +16,35 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
-    int fd;
+    int fd, cnt01;
+    bool isStarted;
     ~MainWindow();
 
+protected:
+    virtual void closeEvent( QCloseEvent *event);
+
 private slots:
-    void on_pushButton_clicked();
+    void on_pushButtonInit_clicked();
 
-    void on_pushButton_3_clicked();
+    void on_pushButtonClose_clicked();
 
-    void on_pushButton_2_clicked();
+    void on_pushButtonOpen_clicked();
 
-    void on_pushButton_4_clicked();
+    void on_pushButtonString_clicked();
 
-    void on_pushButton_5_clicked();
+    void on_pushButtonClear_clicked();
 
-    void on_pushButton_6_clicked();
+    void on_pushButtonPos_clicked();
+
+    void slotTimerAlarm();
+
+    void on_pushButtonExit_clicked();
+
+    void on_pushButtonTest_clicked();
 
 private:
     Ui::MainWindow *ui;
+    QTimer *timer;
 };
 
 #endif // MAINWINDOW_H
